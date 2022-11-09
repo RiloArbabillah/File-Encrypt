b55f99c875be1e1f54b84639a2d5d8c5b938a457
RiloArbabillah <rilo.arbabillah@gmail.com>
2022-11-09 12:35:01 +0800

    wip

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Files;
+use Illuminate\Http\Request;
+
+class FilesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $files = Files::get();
+
+        return view('files.index', [
+            'files' => $files
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('files.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd($request);
+        // return view('files.form');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Files  $files
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Files $files)
+    {
+        dd($files);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Files  $files
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Files $files)
+    {
+        return view('files.form', [
+            'files' => $files
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Files  $files
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Files $files)
+    {
+        dd($request, $files);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Files  $files
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Files $files)
+    {
+        dd($files);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('files')->name('files.')->group(function() {
+    Route::get('/', [FilesController::class, 'index'])->name('index');
+    Route::get('/create', [FilesController::class, 'create'])->name('create');
+    Route::post('/', [FilesController::class, 'store'])->name('store');
+    Route::get('/{files}/show', [FilesController::class, 'show'])->name('show');
+    Route::get('/{files}/edit', [FilesController::class, 'edit'])->name('edit');
+    Route::put('/{files}', [FilesController::class, 'update'])->name('update');
+    Route::delete('/{files}', [FilesController::class, 'destroy'])->name('destroy');
 });
